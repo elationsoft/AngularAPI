23ba823c6c0d90b22e1ac7d30a5105e3625f8d39
elationsoft <deepak@elationsoft.net>
2025-07-19 03:23:28 +0530

    new files

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -9,10 +9,7 @@ namespace minimalCrudAPI.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly string _superbaseConnection;
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+      
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -23,18 +20,6 @@ namespace minimalCrudAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-        }
-
-        [HttpGet("api/GetSuperbaseData")]
         public IActionResult GetSuperbaseData()
         {
             using (var connection = new NpgsqlConnection(_superbaseConnection))
